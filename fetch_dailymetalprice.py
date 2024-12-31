306d54f6b2e6b16de08793f67cb7f0f8cd8908ea
Doosheck <michal.adam.kasperek@gmail.com>
2024-12-31 15:16:57 +0100

    UPD: exporting all prices for one date

--- fetch_dailymetalprice.py
+++ fetch_dailymetalprice.py
@@ -33,60 +33,24 @@ def fetch_metal_prices_firefox(date: str):
 
         html = driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
-        # <table class="table table-striped table-hover table-condensed success">
         table = soup.find('table', {'class': 'table table-striped table-hover table-condensed success'})
-        headers = table.find_all('thead')
-        # Print the table content
-        # print(headers)
-        # print(f"Table {table}")
-        # Print the single td elements of separate tr in new lines. The tbody has id="rate-table"
+        metal_prices = {}
         for row in table.find_all('tr'):
-            for cell in row.find_all('td'):
-                print(cell.text)
-            print("\n")
+            cells = row.find_all('td')
+            if len(cells) >= 3:
+                metal = cells[0].text
+                price_currency = cells[1].text
+                price = price_currency.split(' ')[0]
+                unit = cells[2].text
+                metal_prices[metal] = (price, unit)
+        return metal_prices
     finally:
-        pass
-
-    #     # Locate the table where tbody id = rate-table and extract data
-    #     rows = driver.find_elements(By.CSS_SELECTOR, "#rate-table tr")
-
-    #     # Locate the table and extract data
-    #     rows = driver.find_elements(By.CSS_SELECTOR, "#rate-table tr")
-    #     metal_prices = {}
-    #     print(f"Found {len(rows)} rows")
-    #     print(rows[0].find_elements(By.CSS_SELECTOR, "td"))
-    #     # print the content of the table
-    #     for cell in rows[0].find_elements(By.CSS_SELECTOR, "td"):
-    #         print(cell.text)
-    #     for cell in rows[1].find_elements(By.CSS_SELECTOR, "td"):
-    #         print(cell.text)
-
-
-    #     # print(rows[1].find_elements(By.CSS_SELECTOR, "td"))
-    #     print(driver.find_element(By.CSS_SELECTOR, "#rate-table").text)
-
-
-
-    #     print("\n")
-    #     # <tbody id="rate-table"><tr><td>Aluminum</td><td>1.1617<span class="hidden-xs"> USD</span></td><td><span class="hidden-xs">lb (</span>Pound<span class="hidden-xs">)</span></td><td class="hidden-xs">Dec 24, 2024</td></tr><tr><td>Cobalt</td><td>11.022<span class="hidden-xs"> USD</span></td><td><span class="hidden-xs">lb (</span>Pound<span class="hidden-xs">)</span></td>
-    #     for row in rows:
-    #         cells = row.find_elements(By.CSS_SELECTOR, "td")
-    #         if len(cells) >= 3:
-    #             metal = cells[0].text
-    #             price = cells[1].text
-    #             unit = cells[2].text
-    #             metal_prices[metal] = (price, unit)
-    #             # print(f'{metal}: {price} per {unit}')
-    #     return metal_prices
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
-    # finally:
-    #     # Close the browser window
-    #     driver.quit()
+        driver.quit()
 
 # Example usage
 date = '2024-12-24'
 prices = fetch_metal_prices_firefox(date)
 if prices:
+    print(f'Metal prices for {date}:')
     for metal, (price, unit) in prices.items():
         print(f'{metal}: {price} per {unit}')
