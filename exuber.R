581d22c10d36652bd4abdb836d6c6e9210f372a3
Barbara Bedowska-Sojka <barbara.bedowska-sojka@ue.poznan.pl>
2026-01-20 10:16:47 +0100

    encoding correction

--- exuber.R
+++ exuber.R
@@ -50,90 +50,90 @@ autoplot(est)
 #---- an application to metals----
 
 getwd()
-df_NI <- read.csv("data/ALL_nickel_prices_cubic_spline.csv") #bañka na 1 szeregu NIETFN
+df_NI <- read.csv("data/ALL_nickel_prices_cubic_spline.csv") #ba?ka na 1 szeregu NIETFN
 df_CU <- read.csv("data/ALL_copper_prices_cubic_spline.csv") #nie ma baniek
-df_LI <- read.csv("data/ALL_lithium_prices_cubic_spline.csv") #s¹ bañki na LIDAILY i LISAME
-df_CO <- read.csv("data/ALL_cobalt_prices_cubic_spline.csv") #s¹, du¿o i w tym samym czasie
+df_LI <- read.csv("data/ALL_lithium_prices_cubic_spline.csv") #s? ba?ki na LIDAILY i LISAME
+df_CO <- read.csv("data/ALL_cobalt_prices_cubic_spline.csv") #s?, du?o i w tym samym czasie
 
 
-vis_dat(df_NI) # Pokazuje graficznie, gdzie s¹ braki (NA)
+vis_dat(df_NI) # Pokazuje graficznie, gdzie s? braki (NA)
 vis_dat(df_CU)
 vis_dat(df_LI)
 vis_dat(df_CO)
 
 
-# Konwersja daty i filtrowanie "Czêœci Wspólnej" (dla ka¿dego szeregu oddzielnie)
+# Konwersja daty i filtrowanie "Cz??ci Wsp?lnej" (dla ka?dego szeregu oddzielnie)
 df_clean <- df_NI %>%
   select(-c("NIETFN", "NIWUXI")) %>%
-  mutate(Date = as.Date(Date)) %>%  #sprawdzenie czy R rozumie, ¿e to daty
+  mutate(Date = as.Date(Date)) %>%  #sprawdzenie czy R rozumie, ?e to daty
   na.omit()                         
 
 df_clean <- df_CO %>%
   select(-c("COSMMS", "COCOMX")) %>%
-  mutate(Date = as.Date(Date)) %>%  #sprawdzenie czy R rozumie, ¿e to daty
+  mutate(Date = as.Date(Date)) %>%  #sprawdzenie czy R rozumie, ?e to daty
   na.omit()                         
 
 df_clean <- df_CU %>%
   select(-"CUETFC") %>%
-  mutate(Date = as.Date(Date)) %>%  #sprawdzenie czy R rozumie, ¿e to daty
+  mutate(Date = as.Date(Date)) %>%  #sprawdzenie czy R rozumie, ?e to daty
   na.omit()                         
 
 colnames(df_LI)
 df_clean <- df_LI %>%
   select(-c("LILAMC", "LIEALC", "LIEABG")) %>%
-  mutate(Date = as.Date(Date)) %>%  #sprawdzenie czy R rozumie, ¿e to daty
+  mutate(Date = as.Date(Date)) %>%  #sprawdzenie czy R rozumie, ?e to daty
   na.omit()                         
 
 
 # Sprawdzenie zakresu dat po oczyszczeniu
-print(paste("Pocz¹tek analizy:", min(df_clean$Date)))
+print(paste("Pocz?tek analizy:", min(df_clean$Date)))
 print(paste("Koniec analizy:", max(df_clean$Date)))
 print(paste("Liczba obserwacji:", nrow(df_clean)))
 head(df_clean)
 
 # Przygotowanie macierzy do testu
-# Wyci¹gamy same dane numeryczne (bez kolumny Date)
+# Wyci?gamy same dane numeryczne (bez kolumny Date)
 data_matrix <- df_clean %>% 
-  select(-Date) %>% # Usuwamy kolumnê Date, zostaj¹ tylko szeregi cen
+  select(-Date) %>% # Usuwamy kolumn? Date, zostaj? tylko szeregi cen
   mutate(across(everything(), log))
 
-# Przypisujemy daty jako nazwy wierszy (dla ³adnych wykresów)
+# Przypisujemy daty jako nazwy wierszy (dla ?adnych wykres?w)
 rownames(data_matrix) <- as.character(df_clean$Date)
 
 head(data_matrix)
 
 # Uruchomienie testu (GSADF, SADF, ADF w jednym)
-# To mo¿e chwilê potrwaæ w zale¿noœci od d³ugoœci szeregu
+# To mo?e chwil? potrwa? w zale?no?ci od d?ugo?ci szeregu
 est_results <- radf(data_matrix)
 
-# Wyœwietlenie podsumowania (czy wykryto bañki?)
-summary(est_results) #je¿eli pojawia siê b³¹d, wówczas
-# prawodpodobnie jest to problem z instalacj¹ i trzeba uruchomiæ 3 linie:
+# Wy?wietlenie podsumowania (czy wykryto ba?ki?)
+summary(est_results) #je?eli pojawia si? b??d, w?wczas
+# prawodpodobnie jest to problem z instalacj? i trzeba uruchomi? 3 linie:
 # exuber::install_exuberdata()
 # library(exuberdata)
 # est_results <- radf(data_matrix)
 
 
-# poniewa¿ u mnie exuberdata nie chce siê zaladowaæ,
-# liczymy wartoœci krytyczne (w zastêpstwie pakietu exuberdata)
-# To mo¿e potrwaæ kilka-kilkanaœcie sekund
+# poniewa? u mnie exuberdata nie chce si? zaladowa?,
+# liczymy warto?ci krytyczne (w zast?pstwie pakietu exuberdata)
+# To mo?e potrwa? kilka-kilkana?cie sekund
 wartosci_krytyczne_NI <- radf_mc_cv(n = nrow(data_matrix)) 
 wartosci_krytyczne_CO <- radf_mc_cv(n = nrow(data_matrix)) 
 wartosci_krytyczne_LI <- radf_mc_cv(n = nrow(data_matrix))
 
-# 2. Wyznacz daty i podaj wartoœci krytyczne 
+# 2. Wyznacz daty i podaj warto?ci krytyczne 
 bubble_dates <- datestamp(est_results, cv = wartosci_krytyczne_NI)
 bubble_dates <- datestamp(est_results, cv = wartosci_krytyczne_CO)
 bubble_dates <- datestamp(est_results, cv = wartosci_krytyczne_LI)
 #bubble_dates <- datestamp(est_results)
 
-# o ile s¹ jakieœ bañki, to tu je mo¿na narysowaæ
+# o ile s? jakie? ba?ki, to tu je mo?na narysowa?
 autoplot(bubble_dates) #+
   #facet_wrap(~ series, scales = "free_y") +
-  #labs(title = "Wykryte bañki spekulacyjne na rynkach niklu") +
+  #labs(title = "Wykryte ba?ki spekulacyjne na rynkach niklu") +
   #theme_minimal()
 
-# Poka¿ daty 
+# Poka? daty 
 print(bubble_dates)
 
 # wykres w ggplot (zamiast autoplot)
@@ -146,33 +146,33 @@ df_plot <- as.data.frame(data_matrix) %>%
   mutate(Series = factor(Series, levels = keep_order))
 
 # --- B. PRZYGOTOWANIE BANIEK  ---
-# Pêtla przejdzie przez wyniki i zamieni numery wierszy na daty.
+# P?tla przejdzie przez wyniki i zamieni numery wierszy na daty.
 
 bubble_rects <- data.frame() # Pusty kontener na wyniki
 
 # Pobieramy wektor prawdziwych dat z Twoich danych
 all_dates <- as.Date(rownames(data_matrix))
 
-# Pêtla po ka¿dym szeregu 
+# P?tla po ka?dym szeregu 
 for (name in names(bubble_dates)) {
   
-  # Pobierz tabelê start/koniec dla danego metalu
+  # Pobierz tabel? start/koniec dla danego metalu
   periods <- bubble_dates[[name]]
   
-  # Jeœli wykryto jakieœ bañki (tabela nie jest pusta)
+  # Je?li wykryto jakie? ba?ki (tabela nie jest pusta)
   if (nrow(periods) > 0) {
     
-    # Zamieñ numery wierszy na Daty
+    # Zamie? numery wierszy na Daty
     starts <- all_dates[periods[, "Start"]]
     ends   <- all_dates[periods[, "End"]]
     
-    # Stwórz ma³¹ tabelkê i dodaj do g³ównej
+    # Stw?rz ma?? tabelk? i dodaj do g??wnej
     temp_df <- data.frame(
       Series = name,
       xmin = starts,
       xmax = ends,
-      ymin = -Inf,  # Prostok¹t od samego do³u...
-      ymax = Inf    # ...do samej góry wykresu
+      ymin = -Inf,  # Prostok?t od samego do?u...
+      ymax = Inf    # ...do samej g?ry wykresu
     )
     
     bubble_rects <- rbind(bubble_rects, temp_df)
@@ -182,7 +182,7 @@ if(nrow(bubble_rects) > 0) {
   bubble_rects$Series <- factor(bubble_rects$Series, levels = keep_order)
 }
 
-# Sprawdzamy, czy coœ znalaz³ (je¿eli tak, to bêd¹ daty)
+# Sprawdzamy, czy co? znalaz? (je?eli tak, to b?d? daty)
 print(head(bubble_rects))
 
 # jakie jest minimalne okno obserwacji do testu gsadf
@@ -198,12 +198,12 @@ print(paste("Minimalne okno (w dniach):", min_window))
 #library(patchwork)
 
 bubble_plot_CO <- ggplot() +
-  # WARSTWA 1: Czerwone obszary (bañki)
-  # Rysujemy je TYLKO jeœli bubble_rects nie jest puste
+  # WARSTWA 1: Czerwone obszary (ba?ki)
+  # Rysujemy je TYLKO je?li bubble_rects nie jest puste
   {if(nrow(bubble_rects) > 0) 
     geom_rect(data = bubble_rects, 
               aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
-              fill = "red", alpha = 0.3) # alpha to przezroczystoœæ
+              fill = "red", alpha = 0.3) # alpha to przezroczysto??
   } +
   
   # WARSTWA 2: Ceny (Czarne linie)
@@ -211,18 +211,18 @@ bubble_plot_CO <- ggplot() +
   
   facet_wrap(~ Series, scales = "free_y", ncol = 2) + 
   
-  scale_x_date(date_labels = "%Y", date_breaks = "1 year") + # £adne daty co rok
+  scale_x_date(date_labels = "%Y", date_breaks = "1 year") + # ?adne daty co rok
   labs(title = "",
        #subtitle = ",
        x = "",
        y = "") +
   theme_minimal() +
-  #theme(axis.text.x = element_text(angle = 45, hjust = 1)) # Pochylenie dat, ¿eby siê mieœci³y
+  #theme(axis.text.x = element_text(angle = 45, hjust = 1)) # Pochylenie dat, ?eby si? mie?ci?y
   theme(
-  strip.text = element_text(size = 14, face = "bold"), # Rozmiar i pogrubienie tytu³ów paneli
+  strip.text = element_text(size = 14, face = "bold"), # Rozmiar i pogrubienie tytu??w paneli
   #axis.text.x = element_text(angle = 45, hjust = 1)   # Opcjonalne pochylenie dat
 )
-# zmienic nazwê pliku banki_metal,pdf
+# zmienic nazw? pliku banki_metal,pdf
 ggsave(filename = "graphsR/bubble_nickel.pdf", 
        plot = bubble_plot_NI, 
        width = 12, 
